3771e24370d2cda20827a4b293b0f6fc2dc8a7e3
Greg Fitzgerald <greg@icontrib.org>
2012-10-13 23:44:47 -0700

    cleanup

--- UnassignedVars.hs
+++ UnassignedVars.hs
@@ -26,19 +26,19 @@ type UnassignedVars = [(String, Statement)]
 --   Move the declaration down to just before the variable's first assignment.
 --   If it is never assigned, it is discarded.
 unassignedVars :: Inum Block Block IO a
-unassignedVars = statefulPass unassignedVarsChunk []
+unassignedVars = statefulPass chunk []
 
-unassignedVarsChunk :: Iter Block (StateT UnassignedVars IO) Block
-unassignedVarsChunk = concat `fmap` (dataI >>= mapM unassignedVarsStat)
+chunk :: Iter Block (StateT UnassignedVars IO) Block
+chunk = concat `fmap` (dataI >>= mapM stat)
 
-unassignedVarsStat :: Statement -> Iter Block (StateT UnassignedVars IO) Block
-unassignedVarsStat e@(Declaration _ nm) = do
+stat :: Statement -> Iter Block (StateT UnassignedVars IO) Block
+stat e@(Declaration _ nm) = do
    modify ((nm, e) :)
    return []
-unassignedVarsStat e@(Assignment nm _) = do
+stat e@(Assignment nm _) = do
    xs <- get
    case lookup nm xs of
      Just decl -> modify (delete (nm, e)) >> return [decl, e]
      Nothing -> return [e]
-unassignedVarsStat x = return [x]
+stat x = return [x]
 
