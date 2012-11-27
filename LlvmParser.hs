73293d2b76ba5ec077ed961a4b9dca8e1e872eca
Greg Fitzgerald <greg@icontrib.org>
2012-11-26 22:52:53 -0800

    Migrate most of LlvmParser from iterIO to attoparsec

--- LlvmParser.hs
+++ LlvmParser.hs
@@ -1,68 +1,73 @@
-{-# LANGUAGE Safe #-}
+{-# LANGUAGE Trustworthy #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module LlvmParser where
 
 -- From IterIO package
 import Data.IterIO
-  ( Iter
-  , Inum
+  ( Inum
   , mkInum
   )
-import Data.IterIO.Parse
-  ( (<|>) -- Alternative
-  , (<?>) -- Add a name
+import Data.Attoparsec.ByteString.Lazy
+  ( (<?>) -- Add a name
   , string
-  , many
   , satisfy
-  , eofI
-  , eord
-  , whileI
-  , while1I
-  , optionalI
-  , skipWhileI
-  , skipWhile1I
+  , endOfInput
+  , takeWhile1
+  , skipWhile
   , sepBy
   , sepBy1
+  , Parser
   )
+import qualified Data.Attoparsec.ByteString.Lazy as A
 import Control.Applicative
   ( (<*>) -- Apply
   , (<$>) -- Apply pure
   , (<*)  -- Apply but return the left
   , (*>)  -- Apply but return the right
+  , (<|>)
+  , optional
+  , many
+  , Alternative
   )
-import Data.ListLike
-  ( singleton
+import Data.IterIO.Atto
+  ( atto
+  )
+import Data.Char
+  ( ord
   )
 
 import Block
 
 import qualified Data.ByteString.Lazy.Char8 as L
+import qualified Data.ByteString.Char8 as BC
+
 
 parseFlow :: Inum L.ByteString Module IO a
-parseFlow = mkInum toplevelEntities
+parseFlow = mkInum $ atto toplevelEntities
 
-basicBlock :: Iter L.ByteString IO Block
+basicBlock :: Parser Block
 basicBlock = concat <$> many basicBlock'
 
-basicBlock' :: Iter L.ByteString IO Block
-basicBlock' = singleton <$> (whitespace *> (stat <|> flushStat) <* terminator)
+basicBlock' :: Parser Block
+basicBlock' = (: []) <$> (whitespace *> (stat <|> flushStat) <* terminator)
          <|> terminator *> return []
 
-stat :: Iter L.ByteString IO Statement
+stat :: Parser Statement
 stat       = assignStat
          <|> returnStat
          <|> branchStat
          <|> branchCondStat
          <|> labelStat
 
-branchStat :: Iter L.ByteString IO Statement
+branchStat :: Parser Statement
 branchStat = do
     keyword "br"
     keyword "label"
     Branch <$> identifier
 
 -- br i1 %cond, label %IfEqual, label %IfUnequal
-branchCondStat :: Iter L.ByteString IO Statement
+branchCondStat :: Parser Statement
 branchCondStat = do
     keyword "br"
     keyword "i1"
@@ -75,45 +80,48 @@ branchCondStat = do
     right <- identifier
     return $ BranchCond expr left right
 
-boolLit :: Iter L.ByteString IO Literal
+boolLit :: Parser Literal
 boolLit = (string "true"  >> return (LitBool True))
       <|> (string "false" >> return (LitBool False))
 
 
-flushStat :: Iter L.ByteString IO Statement
+flushStat :: Parser Statement
 flushStat = string ":flush" *> return Flush
          <?> "flush signal"
 
-terminator :: Iter L.ByteString IO ()
-terminator = skipWhileI isHorizSpace <* optionalI comment <* satisfy isTerminator
-         <|> eofI
+terminator :: Parser ()
+terminator = skipWhile isHorizSpace <* optional comment <* satisfy isTerminator
+         <|> endOfInput
          <?> "terminator"
 
 isHorizSpace :: (Enum a, Eq a) => a -> Bool
 isHorizSpace s = s == eord ' ' || s == eord '\t'
 
+eord :: Enum a => Char -> a
+eord = toEnum . ord
+
 isTerminator :: (Enum a, Eq a) => a -> Bool
 isTerminator s = s == eord '\n'
 
-comment :: Iter L.ByteString IO L.ByteString
-comment = satisfy (== eord ';') *> whileI (not . isTerminator)
+comment :: Parser BC.ByteString
+comment = satisfy (== eord ';') *> A.takeWhile (not . isTerminator)
 
-assignStat :: Iter L.ByteString IO Statement
+assignStat :: Parser Statement
 assignStat = Assignment <$> identifier <*> (token "=" *> expression)
          <?> "assignment"
 
-returnStat :: Iter L.ByteString IO Statement
+returnStat :: Parser Statement
 returnStat = Return <$> (keyword "ret" *> llvmType) <*> expression
          <?> "return statement"
 
-expression :: Iter L.ByteString IO Expr
+expression :: Parser Expr
 expression = ExprVar <$> identifier
          <|> ExprConstant . LitInteger <$> integer
          <|> addExpr
          <|> phiExpr
          <?> "expression"
 
-addExpr :: Iter L.ByteString IO Expr
+addExpr :: Parser Expr
 addExpr = do
     keyword "add"
     ty <- llvmType
@@ -122,27 +130,27 @@ addExpr = do
     e2 <- expression
     return $ ExprAdd ty e1 e2
 
-phiExpr :: Iter L.ByteString IO Expr
+phiExpr :: Parser Expr
 phiExpr = do
     keyword "phi"
     ty <- llvmType
     es <- brackets phiSource `sepBy1` comma
     return $ ExprPhi ty es
 
-phiSource :: Iter L.ByteString IO (Expr, String)
+phiSource :: Parser (Expr, String)
 phiSource = (,) <$> (expression <* comma) <*> identifier
 
-integer :: Iter L.ByteString IO Integer
-integer = read . L.unpack <$> while1I isNum
+integer :: Parser Integer
+integer = read . BC.unpack <$> takeWhile1 isNum
 
-globalVar :: Iter L.ByteString IO String
-globalVar = L.unpack <$> (string "@" *> while1I isAlphaNumOrUnder)
+globalVar :: Parser String
+globalVar = BC.unpack <$> (string "@" *> takeWhile1 isAlphaNumOrUnder)
 
-identifier :: Iter L.ByteString IO String
-identifier = L.unpack <$> (string "%" *> while1I isAlphaNumOrUnder)
+identifier :: Parser String
+identifier = BC.unpack <$> (string "%" *> takeWhile1 isAlphaNumOrUnder)
 
-labelStat :: Iter L.ByteString IO Statement
-labelStat = (Label . L.unpack) <$> (while1I isAlphaNumOrUnder <* string ":")
+labelStat :: Parser Statement
+labelStat = (Label . BC.unpack) <$> (takeWhile1 isAlphaNumOrUnder <* string ":")
 
 isLetterOrUnder :: (Enum a, Ord a) => a -> Bool
 isLetterOrUnder s = s >= eord 'a' && s <= eord 'z' || s >= eord 'A' && s <= eord 'Z' || s == eord '_'
@@ -154,34 +162,34 @@ isNum :: (Enum a, Ord a) => a -> Bool
 isNum s = s >= eord '0' && s <= eord '9'
 
 -- Requires some whitespace after keyword
-keyword :: String -> Iter L.ByteString IO ()
+keyword :: BC.ByteString -> Parser ()
 keyword s = string s >> whitespace1
 
 -- Optional whitespace on either side of a given string
-token :: String -> Iter L.ByteString IO ()
+token :: BC.ByteString -> Parser ()
 token s = whitespace >> string s >> whitespace
 
-whitespace :: Iter L.ByteString IO ()
-whitespace = skipWhileI isWhite
+whitespace :: Parser ()
+whitespace = skipWhile isWhite
 
-whitespace1 :: Iter L.ByteString IO ()
-whitespace1 = skipWhile1I isWhite
+whitespace1 :: Parser ()
+whitespace1 = takeWhile1 isWhite *> return ()
 
 isWhite :: (Enum a, Eq a) => a -> Bool
 isWhite s = s == eord ' ' || s == eord '\n' || s == eord '\t' || s == eord '\r'
 
-toplevelEntities :: Iter L.ByteString IO Module
+toplevelEntities :: Parser Module
 toplevelEntities = (do
     x <- whitespace *> toplevelEntity
     return [x])
  <|> (terminator *> return [])
 
-toplevelEntity :: Iter L.ByteString IO ToplevelEntity
+toplevelEntity :: Parser ToplevelEntity
 toplevelEntity = function
              <|> target
              <?> "top-level entity"
 
-target :: Iter L.ByteString IO ToplevelEntity
+target :: Parser ToplevelEntity
 target = do
     keyword "target"
     nm <- targetName
@@ -190,22 +198,22 @@ target = do
     s <- stringLiteral
     return $ Target nm s
 
-targetName :: Iter L.ByteString IO String
-targetName = L.unpack <$> (string "datalayout" <|> string "triple")
+targetName :: Parser String
+targetName = BC.unpack <$> (string "datalayout" <|> string "triple")
 
-stringLiteral :: Iter L.ByteString IO String
+stringLiteral :: Parser String
 stringLiteral = do
     _ <- string "\""
-    s <- while1I (eord '"' /=)
+    s <- takeWhile1 (eord '"' /=)
     _ <- string "\""
-    return $ L.unpack s
+    return $ BC.unpack s
 
 --define [linkage] [visibility]
 --       [cconv] [ret attrs]
 --       <ResultType> @<FunctionName> ([argument list])
 --       [fn Attrs] [section "name"] [align N]
 --       [gc] { ... }
-function :: Iter L.ByteString IO ToplevelEntity
+function :: Parser ToplevelEntity
 function = do
     keyword "define"
     ty <- llvmType
@@ -217,33 +225,33 @@ function = do
     bb   <- braces basicBlock
     return $ Function ty nm args as bb
 
-parens :: Iter L.ByteString IO a -> Iter L.ByteString IO a
+parens :: Parser a -> Parser a
 parens p = string "(" *> whitespace *> p <* whitespace <* string ")"
 
-braces :: Iter L.ByteString IO a -> Iter L.ByteString IO a
+braces :: Parser a -> Parser a
 braces p = string "{" *> whitespace *> p <* whitespace <* string "}"
 
-brackets :: Iter L.ByteString IO a -> Iter L.ByteString IO a
+brackets :: Parser a -> Parser a
 brackets p = string "[" *> whitespace *> p <* whitespace <* string "]"
 
-argument :: Iter L.ByteString IO String
+argument :: Parser String
 argument = do
     whitespace
     _ty <- llvmType
     identifier
 
-comma :: Iter L.ByteString IO ()
+comma :: Parser ()
 comma = whitespace *> string "," *> whitespace
 
-llvmType :: Iter L.ByteString IO String
+llvmType :: Parser String
 llvmType = do
     ty  <- intType
-    ptr <- (L.unpack <$> string "*") <|> return ""
+    ptr <- (BC.unpack <$> string "*") <|> return ""
     whitespace1
     return $ ty ++ ptr
 
-intType :: Iter L.ByteString IO String
-intType = L.unpack <$> (
+intType :: Parser String
+intType = BC.unpack <$> (
                     string "i1"
                 <|> string "i8"
                 <|> string "i16"
@@ -252,8 +260,8 @@ intType = L.unpack <$> (
                 <|> string "i128"
                 )
 
-functionAttribute :: Iter L.ByteString IO String
-functionAttribute = L.unpack <$> (
+functionAttribute :: Parser String
+functionAttribute = BC.unpack <$> (
                     string "address_safety"
                 <|> string "alignstack"
                 <|> string "alwaysinline"
